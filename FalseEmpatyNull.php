d70de4ab7fd8d5b6f1b4cb53ff34ad304c249883
tiohs <hamiltonsilva551@gmail.com>
2021-09-28 15:44:13 +0100

    False null empaty

---
+++ FalseEmpatyNull.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>False Null Empaty </title>
+</head>
+<body>
+  is_null - Verifica valores é null com is_null 
+  empaty - verifica se a string está fazia ou vazio 
+  false - é mesmo false 
+
+  empaty posso verificar null e variavel vazia 
+</body>
+</html>
