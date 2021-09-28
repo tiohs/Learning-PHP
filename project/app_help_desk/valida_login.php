8cb7622993fafa094b808d29b53fed484830c6c7
tiohs <hamiltonsilva551@gmail.com>
2021-09-28 22:49:24 +0100

    validardados

--- project/app_help_desk/valida_login.php
+++ project/app_help_desk/valida_login.php
@@ -1,4 +1,17 @@
 <?php
-  echo $_POST['email'];
+  $user_auth = false;
+
+  $users_data = [
+    ['email' => 'hs@hs', 'password' => '1234'],
+    ['email' => 'hs@hs', 'password' => '1234']
+  ];
+
+  foreach($users_data as $user) {
+    if($user['email'] === $_POST['email'] and $user['password'] === $_POST['password']){
+      $user_auth = true;
+    }
+  }
+  
+  echo $user_auth ? 'Active' : header('Location: index.php?login=erro');
 
 ?>
