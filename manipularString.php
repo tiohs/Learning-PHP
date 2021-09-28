af0075e5bb2bd24809632027fc1eaf8e2faae639
tiohs <hamiltonsilva551@gmail.com>
2021-09-28 13:55:22 +0100

    Função de manipulação de String

--- manipularString.php
+++ manipularString.php
@@ -7,6 +7,43 @@
   <title>Manipular String</title>
 </head>
 <body>
-  
+  <h1> Manipulação de String </h1>
+  <?php
+    echo "Transforma todos os dados em minusculo <br>"; 
+    echo strtolower('Hamilton Silva');
+    
+  ?>
+  <br>
+  <br>
+  <?php
+    echo "Transforma todos os dados em maiúsculos <br>"; 
+    echo strtoupper('Hamilton Silva');
+  ?>
+   <br>
+    <br>
+  <?php
+    echo "Transforma todos a primeira letra em maiúsculos <br>"; 
+    echo ucfirst('hamilton') . " " . ucfirst('silva');
+    
+  ?>
+  <br>
+  <br>
+  <?php
+    echo "Quantidade de caracter <br>"; 
+    echo strlen('hamilton'). " " . ucfirst('hamilton');  
+  ?>
+  <br>
+  <br>
+  <?php
+    echo "Substituir uma cadeia de carater por um outro  <br>"; 
+    echo str_replace(' ', ',', 'Hamilton Silva gosta de comer');  
+  ?>
+  <br>
+  <br>
+  <?php
+    echo "Retorna a parte de uma string  <br>"; 
+    echo substr("Hamilton", 0, 3);  
+  ?>
+ 
 </body>
 </html>
