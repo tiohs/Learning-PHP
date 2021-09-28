63b417e43519126a6bc2ccdd30df48d8a86b55d2
tiohs <hamiltonsilva551@gmail.com>
2021-09-28 14:04:38 +0100

    funcMatematica

---
+++ funcMatematica.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Funções Matematica</title>
+</head>
+<body>
+  <h1> Funcções Matematica </h1>
+  <br>
+  <h2>Cail - Arredendo o valor para cima </h2>
+  <strong>Valor 12.4 :</strong>   <?= ceil(12.4)?>
+  <br>
+  <h2>floor - Arredendo o valor para baixo </h2>
+  <strong>Valor 12.4 :</strong>   <?= floor(12.4)?>
+  <br>
+  <h2>round - Arredendo o valor com base nas casa decimais </h2>
+  <strong>Valor 12.6 :</strong>   <?= round(12.6)?>
+  <br>
+  <h2>rand - Gera um inteiro aleatório </h2>
+  <strong>Valor : </strong>  <?= rand()?>
+  <br>
+  <h2>sqrt - Retorna raiz quadrada  </h2>
+  <strong>Valor 9: </strong>  <?= sqrt(9)?>
+</body>
+</html>
