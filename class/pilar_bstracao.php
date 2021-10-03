9a52d42c1b232ba3a534518ac0aadff4cfd59d60
tiohs <hamiltonsilva551@gmail.com>
2021-10-03 21:12:12 +0100

    add model atrributes, Metode

---
+++ class/pilar_bstracao.php
@@ -0,0 +1,23 @@
+<?php
+  // Model
+
+  class Officer {
+    // attribute
+
+    public $name = null;
+    public $phoneNumber = null;
+    public $childNumber = null;
+
+    // Metodos 
+
+    function getCodFunc () {
+
+    }
+
+    function setNumChild () {
+      
+    }
+  }
+
+
+?>
